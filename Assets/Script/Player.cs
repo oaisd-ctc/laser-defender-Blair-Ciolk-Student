323aa3076367247cc78ec0b182a3127e14fa6228
Blair-Ciolk-Student <112421414+Blair-Ciolk-Student@users.noreply.github.com>
2023-04-28 14:41:03 -0400

    Commit 6
    Added lots of stuff

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -15,7 +15,12 @@ public class Player : MonoBehaviour
     Vector2 minBounds;
     Vector2 maxBounds;
 
+    Shooter shooter;
 
+    void Awake()
+    {
+        shooter = GetComponent<Shooter>();
+    }
 
     private void Start()
     {
@@ -48,6 +53,15 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
-        Debug.Log(rawInput);
     }
+
+    void OnFire(InputValue value)
+    {
+        if(shooter != null)
+        {
+            shooter.isFiring = value.isPressed;
+        }
+    }
+
+
 }
